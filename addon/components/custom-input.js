5b581046bb4fc154994fe700462bcc0d15bed020
Mariraj K <mariraj.kk@zohocorp.com>
2023-07-12 17:26:45 +0530

    Improved validation one more time

--- addon/components/custom-input.js
+++ addon/components/custom-input.js
@@ -4,4 +4,9 @@ import layout from '../templates/components/custom-input';
 export default Component.extend({
   layout,
   classNames: "custom-input",
+  actions:{
+    onInput(ev){
+      this.onInput(ev.target.value);
+    }
+  }
 });
